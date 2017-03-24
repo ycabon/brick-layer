10582f124a72f25c22421bd8ec5cf20600437614
Yann Cabon <ycabon@esri.com>
2017-03-24 09:41:52 -0700

    clean up

--- src/layers/BrickLayer.js
+++ src/layers/BrickLayer.js
@@ -1,80 +1,16 @@
 define([
   "require",
   "esri/request",
-  "esri/layers/BaseTileLayer"
+  "esri/layers/BaseTileLayer",
+
+  "./colorUtils"
 ],
 function(
   require,
   esriRequest,
-  BaseTileLayer
+  BaseTileLayer,
+  colorUtils
 ) {
-
-  /**
-   * Snap a rgb color to a palette
-   */
-  function colorToPalette(out, hsl, palette) {
-    // convert the color to HSL to snap the color in linear space
-
-    // find the closest hue
-    var distances = palette.map(function(color) {
-      return Math.abs(Math.sqrt(Math.pow(color[0] - hsl[0], 2) + Math.pow(color[1] - hsl[1], 2) + Math.pow(color[2] - hsl[2], 2)));
-    })
-    var smallest = Math.min.apply(null, distances);
-    var closestColor = palette[distances.indexOf(smallest)];
-
-    out[0] = closestColor[0];
-    out[1] = closestColor[1];
-    out[2] = closestColor[2];
-
-    return out;
-  }
-
-  function rgb2hsl(out, rgb) {
-    // http://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/
-    var r = rgb[0] / 255;
-    var g = rgb[1] / 255;
-    var b = rgb[2] / 255;
-
-    var min = Math.min(r, g, b);
-    var max = Math.max(r, g, b);
-    var delta = max - min;
-    
-    out[0] = out[1] = out[2] = 0;
-
-    // Luminescance
-    out[2] = (min + max) * 0.5;
-
-    // calculate saturation
-    // no saturation if the rgb values are the same
-    if (delta !== 0) {
-      if (out[2] < 0.5) {
-        out[1] = delta / (max + min);
-      }
-      else {
-        out[1] = delta / (2.0 - delta);
-      }
-    }
-
-    // calculate hue
-    if (delta > 0) {
-      if (r === max) {
-        out[0] = (g - b) / delta;
-      }
-      else if (g === max) {
-        out[0] = 2.0 + (b - r) / delta;
-      }
-      else if (b === max) {
-        out[0] = 4.0 + (r - g) / delta;
-      }
-
-      out[0] = out[0] * 60;
-    }
-
-    out[1] = Math.round(out[1] * 100);
-    out[2] = Math.round(out[2] * 100);
-
-    return out;
-  }
 
   var BrickLayer = BaseTileLayer.createSubclass({
     declaredClass: "BrickLayer",
@@ -95,10 +31,12 @@ function(
           }
 
           return this.palette.map(function(color) {
-            return rgb2hsl([0, 0, 0], color);
+            return colorUtils.rgb2hsl([0, 0, 0], color);
           })
         }
       },
+
+      filter: "",
 
       title: {
         aliasOf: "layer.title"
@@ -108,7 +46,7 @@ function(
     load: function() {
       this.addResolvingPromise(this.layer.load());
       this.addResolvingPromise(
-        esriRequest(require.toUrl("./bricktop.png"), {
+        esriRequest(require.toUrl("./legotop.png"), {
           responseType: "image",
           allowImageDataAccess: true
         })
@@ -144,17 +82,19 @@ function(
         for (var r = 0; r < numBlocks; r++) {
           for (var c = 0; c < numBlocks; c++) {
             var color = this.sampleAverageColor([0, 0, 0], imageData, c * brickSize, r * brickSize, brickSize, brickSize);
-            // rgb2hsl(color, color);
+            
+            colorUtils.rgb2hsl(color, color);
+            // if (this.hslPalette) {
+            //   colorUtils.colorToPalette(color, color, this.hslPalette);
+            // }
+            context.fillStyle = "hsl(" + color[0] + "," + color[1] + "%," + color[2] + "%)";
+            // context.fillStyle = "rgb(" + color[0] + "," + color[1] + "," + color[2] + ")";
 
-            // color[1] *= 4;
-            // snap to the 
-            if (this.hslPalette) {
-              colorToPalette(color, color, this.hslPalette);
-            }
+            // if (this.filter) {
+            //   context.filter = this.filter;
+            // }
 
             // Draw the color
-            // context.fillStyle = "hsl(" + color[0] + "," + color[1] + "%," + color[2] + "%)";
-            context.fillStyle = "rgb(" + color[0] + "," + color[1] + "," + color[2] + ")";
             context.fillRect(c * brickSize, r * brickSize, brickSize, brickSize);
 
             // Draw the top
---
+++ src/layers/colorUtils.js
@@ -0,0 +1,76 @@
+define([
+  "dojox/color"
+], function() {
+  /**
+   * Snap a rgb color to a palette
+   */
+  function colorToPalette(out, hsl, palette) {
+    // convert the color to HSL to snap the color in linear space
+
+    // find the closest hue
+    var distances = palette.map(function(color) {
+      return Math.abs(Math.sqrt(Math.pow(color[0] - hsl[0], 2) + Math.pow(color[1] - hsl[1], 2) + Math.pow(color[2] - hsl[2], 2)));
+    })
+    var smallest = Math.min.apply(null, distances);
+    var closestColor = palette[distances.indexOf(smallest)];
+
+    out[0] = closestColor[0];
+    out[1] = closestColor[1];
+    out[2] = closestColor[2];
+
+    return out;
+  }
+
+  function rgb2hsl(out, rgb) {
+    // http://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/
+    var r = rgb[0] / 255;
+    var g = rgb[1] / 255;
+    var b = rgb[2] / 255;
+
+    var min = Math.min(r, g, b);
+    var max = Math.max(r, g, b);
+    var delta = max - min;
+    
+    out[0] = out[1] = out[2] = 0;
+
+    // Luminescance
+    out[2] = (min + max) * 0.5;
+
+    // calculate saturation
+    // no saturation if the rgb values are the same
+    if (delta !== 0) {
+      if (out[2] < 0.5) {
+        out[1] = delta / (2 * out[2]);
+      }
+      else {
+        out[1] = delta / (2 - 2 * out[2]);
+      }
+    }
+
+    // calculate hue
+    if (delta > 0) {
+      if (r === max) {
+        out[0] = (g - b) / delta;
+      }
+      else if (g === max) {
+        out[0] = 2.0 + (b - r) / delta;
+      }
+      else if (b === max) {
+        out[0] = 4.0 + (r - g) / delta;
+      }
+
+      out[0] = out[0] * 60;
+    }
+
+    out[1] = Math.round(out[1] * 100);
+    out[2] = Math.round(out[2] * 100);
+
+    return out;
+  }
+
+  return {
+    rgb2hsl: rgb2hsl,
+    colorToPalette: colorToPalette
+  };
+
+})
